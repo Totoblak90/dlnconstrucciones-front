24ebc3c70c0388cca745700ff176a83b5097c954
maf248 <maf_248@hotmail.com>
2021-11-04 22:58:55 -0300

    Agregue nodemailer y configure basicamente el endpoint /contact para que mande mails mediante una cuenta en gmail. Funciona correctamente, aunque hace falta mejorarlo.

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -1,51 +1,83 @@
-const { validationResult } = require('express-validator');
+const {
+  validationResult
+} = require('express-validator');
+const nodemailer = require("nodemailer");
 
 module.exports = {
-    index: (req, res, next) => {
-        var response = {
+  index: (req, res, next) => {
+    var response = {
+      meta: {
+        status: 200,
+      },
+      data: {
+        message: 'Contact Controller here (endpoint POST para form de contacto, enviar a Mail!! Paquete de node? y Dashboard de Admin?)'
+      }
+    }
+    res.json(response)
+  },
+  contact: (req, res, next) => {
+
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+
+      return res.json({
+        meta: {
+          status: 401
+        },
+        data: {
+          errors: errors.errors,
+          body: req.body
+        }
+      });
+
+    } else {
+
+      // async..await is not allowed in global scope, must use a wrapper
+      async function main() {
+        var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'lnconstrucciones0@gmail.com',
+            pass: 'pruebanodemailer'
+          }
+        });
+
+        var mailOptions = {
+          from: 'lnconstrucciones0@gmail.com',
+          to: `${req.body.email}`,
+          subject: 'Sending Email using Node.js',
+          text: `${req.body.comment}`
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+      }
+
+      main()
+        .then(
+        
+          res.json({
             meta: {
-                status: 200,
+              status: 200,
             },
             data: {
-                message: 'Contact Controller here (endpoint POST para form de contacto, enviar a Mail!! Paquete de node? y Dashboard de Admin?)'
+              email: req.body.email,
+              comment: req.body.comment,
+              message: 'Email Sent Correctly'
             }
-        }
-        res.json(response)
-    },
-    contact: (req, res, next) => {
+          })
+          
+        )
+        .catch(console.error);
 
-        let errors = validationResult(req);
+    }
 
-        if (!errors.isEmpty()) {
-
-            return res.json({
-                meta: {
-                    status: 401
-                },
-                data: {
-                    errors: errors.errors,
-                    body: req.body
-                }
-            });
-
-        } else {
-            // Implementar nodemailer para formulario de contacto
-            console.log(`email: ${req.body.email}`)
-            console.log(`comment: ${req.body.comment}`)
-            var response = {
-                meta: {
-                    status: 200,
-                },
-                data: {
-                    email: req.body.email,
-                    comment: req.body.comment,
-                    message: 'Contact Controller here (endpoint POST para form de contacto) - Falta implementar nodemailer por back y re-captcha por front'
-                }
-            }
-            res.json(response)
-
-        }
-
-    },
+  },
 
 }
