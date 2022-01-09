8077cbe9fe6dbc337161ab6d933bf0461cad8aa0
Tobias Blaksley <tobias.blaksleyiraola@cordialfinanciera.com.ar>
2022-01-09 13:22:02 -0300

    refactor en el modulo de servicios del mainModule

--- src/app/modules/main/pages/servicios/servicios.component.ts
+++ src/app/modules/main/pages/servicios/servicios.component.ts
@@ -1,64 +1,70 @@
 import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
-import { map, takeUntil } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
-import { Services, ServicesData } from '../../interfaces/http/services.interface';
+import {
+  Services,
+  ServicesData,
+} from '../../interfaces/http/services.interface';
 import { environment } from 'src/environments/environment';
 import { PresentationCard } from '../../interfaces/presentation-card.interface';
 import { HttpService } from 'src/app/services/http.service';
+import {
+  unknownErrorAlert,
+  noConnectionAlert,
+} from '../../../../helpers/alerts';
 
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.scss']
+  styleUrls: ['./servicios.component.scss'],
 })
 export class ServiciosComponent implements OnInit, OnDestroy {
-
   public servicios: ServicesData[] = [];
   public serviciosComplete!: Services;
   public servicesToPresent: PresentationCard[] = [];
   private destroy$: Subject<any> = new Subject();
 
-
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) {}
 
   ngOnInit(): void {
     this.subscribeToServices();
-
   }
 
   private subscribeToServices(): void {
-    this.httpService.getAllServices()
-    .pipe(
-      takeUntil(this.destroy$),
-      map((servs: Services) => {
-        this.serviciosComplete = servs
-        return servs.data
-      })
-    )
-    .subscribe( (servData: ServicesData[]) => {
-      this.servicios = servData;
-      this.servicios.forEach(servicio => {
-        servicio.url = `/main/servicios/${servicio.id}`
-        servicio.image = `${environment.API_IMAGE_URL}/${servicio.image}`
-      })
-      this.mapServicesToPresent();
-    })
+    this.httpService
+      .getAllServices()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (servicios: Services) => {
+          if (servicios.meta.status.toString().includes('20')) {
+            this.serviciosComplete = servicios;
+            this.servicios = servicios.data;
+            this.servicios.forEach((servicio) => {
+              servicio.url = `/main/servicios/${servicio.id}`;
+              servicio.image = `${environment.API_IMAGE_URL}/${servicio.image}`;
+            });
+            this.mapServicesToPresent();
+          } else {
+            unknownErrorAlert(servicios);
+          }
+        },
+        (err) => noConnectionAlert(err)
+      );
   }
 
   private mapServicesToPresent(): void {
-    this.servicios.forEach( service => {
+    this.servicios.forEach((service) => {
       this.servicesToPresent.push({
         titulo: service.title,
         urlFoto: service.image,
         ruta: service.url,
-        sendDataByRoute: false
-      })
-    })
+        sendDataByRoute: false,
+      });
+    });
   }
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
   }
-
 }
--- src/app/modules/main/pages/servicios/tipo-servicio/tipo-servicio.component.ts
+++ src/app/modules/main/pages/servicios/tipo-servicio/tipo-servicio.component.ts
@@ -10,6 +10,11 @@ import { Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { PresentationCard } from '../../../interfaces/presentation-card.interface';
 import { HttpService } from 'src/app/services/http.service';
+import { takeUntil } from 'rxjs/operators';
+import {
+  unknownErrorAlert,
+  noConnectionAlert,
+} from '../../../../../helpers/alerts';
 
 @Component({
   selector: 'app-tipo-servicio',
@@ -26,33 +31,44 @@ export class TipoServicioComponent implements OnInit, OnDestroy {
 
   constructor(
     private httpService: HttpService,
-    private activeRoute: ActivatedRoute,
+    private activeRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+    this.getTypeOfService();
+  }
 
+  private getTypeOfService(): void {
     const serviceId = this.activeRoute.snapshot.params.servicioId;
-    this.httpService.getOneService(serviceId).subscribe((serv) => {
-      this.tipoServicioComplete = serv;
-      this.tipoServicio = serv?.data;
-      this.tipoServicioContents = serv?.data?.Contents;
-      this.tipoServicioPictures = serv?.data?.Pictures;
-
-      this.tipoServicioPictures?.forEach((picture) => {
-        picture.picture = `${environment.API_IMAGE_URL}/${picture.picture}`;
-      });
-
-      this.mapServicesToPresent();
-    });
+    this.httpService
+      .getOneService(serviceId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (serv: TipoServicio) => {
+          if (serv.meta.status.toString().includes('20')) {
+            this.tipoServicioComplete = serv;
+            this.tipoServicio = serv?.data;
+            this.tipoServicioContents = serv?.data?.Contents;
+            this.tipoServicioPictures = serv?.data?.Pictures;
+            this.tipoServicioPictures?.forEach((picture) => {
+              picture.picture = `${environment.API_IMAGE_URL}/${picture.picture}`;
+            });
+            this.mapServicesToPresent();
+          } else {
+            unknownErrorAlert(serv);
+          }
+        },
+        (err) => noConnectionAlert(err)
+      );
   }
 
   private mapServicesToPresent(): void {
-    this.tipoServicioPictures?.forEach( picture => {
+    this.tipoServicioPictures?.forEach((picture) => {
       this.picturesToPresent.push({
         urlFoto: picture.picture,
-        openModal: true
-      })
-    })
+        openModal: true,
+      });
+    });
   }
 
   ngOnDestroy(): void {
