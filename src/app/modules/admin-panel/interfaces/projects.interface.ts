6ae3ef8b39fcedd801172935d7bdb35225cfa7b9
Tobias Blaksley <tobias.blaksleyiraola@cordialfinanciera.com.ar>
2022-01-16 15:28:19 -0300

    resolví el problema que tenía con la galería del usuario

--- src/app/modules/admin-panel/interfaces/projects.interface.ts
+++ src/app/modules/admin-panel/interfaces/projects.interface.ts
@@ -4,3 +4,10 @@ export interface ProjectPaymentsReq {
   receipt: string;
   datetime: string;
 }
+
+export interface Galeria {
+  base64: string;
+  project_id: number;
+  type: string;
+  asset_id: number;
+}
--- src/app/modules/main/pages/auth/components/proyects/proyects.component.ts
+++ src/app/modules/main/pages/auth/components/proyects/proyects.component.ts
@@ -1,12 +1,6 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  Input,
-  Output,
-  ViewChild,
-} from '@angular/core';
-import { customMessageAlert } from 'src/app/helpers/alerts';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 import Swal from 'sweetalert2';
 import { User } from '../../../../../../models/user.model';
 import {
@@ -14,25 +8,75 @@ import {
   ProyectAssets,
 } from '../../../../../admin-panel/interfaces/users.interface';
 import { ProjectsService } from '../../../../../admin-panel/services/projects.service';
+import { unknownErrorAlert } from '../../../../../../helpers/alerts';
+import { Galeria } from '../../../../../admin-panel/interfaces/projects.interface';
 
 @Component({
   selector: 'app-proyects',
   templateUrl: './proyects.component.html',
   styleUrls: ['./proyects.component.scss'],
 })
-export class ProyectsComponent {
-  @Input() public user!: User;
+export class ProyectsComponent implements OnInit {
+  @Input() public set setProyectos(user: User) {
+    if (user && user.projects) {
+      this.proyectos = user.projects;
+    }
+  }
   @Output() public onDownloadCashflow: EventEmitter<string> =
     new EventEmitter();
-  private assetCallCounter: number = 0;
+
+  public proyectos: Project[] = [];
+  public assets: Galeria[] = [];
+  private destroy$: Subject<boolean> = new Subject();
 
   constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit(): void {
+      this.setAssets()
+  }
+
+  private setAssets(): void {
+    this.proyectos?.forEach((proj) => {
+      proj.Assets.forEach((asset) => {
+        this.projectsService
+          .getAssetsDeUnProyecto(asset.asset)
+          .pipe(takeUntil(this.destroy$))
+          .subscribe({
+            next: (blob) => {
+              const reader = new FileReader();
+              reader.readAsDataURL(blob);
+              reader.onload = () => {
+                this.assets.push({
+                  base64: reader.result as string,
+                  project_id: proj.id,
+                  type: blob.type,
+                  asset_id: asset.id,
+                });
+              };
+            },
+            error: (err) => unknownErrorAlert(err),
+          });
+      });
+    });
+  }
+
+  public showGalleryCard(asset: Galeria, proyecto: Project): boolean {
+    return asset.project_id === proyecto.id;
+  }
+
+  public showGalleryImage(asset: Galeria, proyecto: Project): boolean {
+    return asset.type.includes('image') && asset.project_id === proyecto.id;
+  }
+
+  public showGalleryVideo(asset: Galeria, proyecto: Project): boolean {
+    return asset.type.includes('video') && asset.project_id === proyecto.id;
+  }
 
   public get encabezados(): string[] {
     let propiedadesDePayments: string[] = [];
     const encabezados = [];
 
-    this.user.projects?.forEach((pr) =>
+    this.proyectos?.forEach((pr) =>
       pr.Payments?.forEach((pay) => (propiedadesDePayments = Object.keys(pay)))
     );
     if (propiedadesDePayments?.length) {
@@ -60,8 +104,8 @@ export class ProyectsComponent {
 
   public get progressBarStyle(): string[] {
     const pb_Style: string[] = [];
-    this.user?.projects?.forEach((pr) => {
-      const num: number = this.setPers(pr.total, pr.balance);
+    this.proyectos?.forEach((pr) => {
+      const num: number = this.setPorcentaje(pr.total, pr.balance);
       pb_Style.push(`width: ${num}%`);
     });
     return pb_Style;
@@ -69,54 +113,24 @@ export class ProyectsComponent {
 
   public get porcentaje(): number[] {
     const porcentajes: number[] = [];
-    this.user?.projects?.forEach((pr) => {
-      const num: number = this.setPers(pr.total, pr.balance);
+    this.proyectos?.forEach((pr) => {
+      const num: number = this.setPorcentaje(pr.total, pr.balance);
       porcentajes.push(num);
     });
     return porcentajes;
   }
 
-  private setPers(total: number, pagado: number): number {
+  private setPorcentaje(total: number, pagado: number): number {
     let result: number = total - pagado;
     result = (result * 100) / total;
     return Math.round(result);
-  }
-
-  public setAssetUrl(proyecto: Project, asset: ProyectAssets): void {
-    let proj: Project | undefined;
-    let archivo: ProyectAssets | undefined;
-    let countErrors: number = 0;
-    if (this.assetCallCounter !== proyecto.Assets.length) {
-      proj = this.user.projects?.find((p) => p.id === proyecto.id);
-      archivo = proj?.Assets.find((a) => a.id === asset.id);
-      this.assetCallCounter++;
-      if (archivo?.asset) {
-        this.projectsService.getAssetsDeUnProyecto(archivo?.asset).subscribe({
-          next: (blob) => this.setImageOrVideoSrcAttribute(blob, asset),
-          error: () => {
-            countErrors++;
-            if (
-              this.assetCallCounter === proyecto.Assets.length &&
-              countErrors > 0
-            ) {
-              customMessageAlert(
-                'Error',
-                'Tuvimos un error desconocido y no pudimos cargar uno o ningún archivo de tu galería de proyecto. Te pedimos recargues la página o esperes un tiempo',
-                'OK',
-                'error'
-              );
-            }
-          },
-        });
-      }
-    }
   }
 
   private setImageOrVideoSrcAttribute(blob: Blob, asset: ProyectAssets): void {
     const reader = new FileReader();
     reader.readAsDataURL(blob);
     reader.onload = () => {
-      this.user.projects?.forEach((proj) => {
+      this.proyectos?.forEach((proj) => {
         proj.Assets.forEach((ar) => {
           if (ar.id === asset.id) {
             ar.asset = reader.result as string;
